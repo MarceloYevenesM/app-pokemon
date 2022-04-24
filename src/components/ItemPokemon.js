a2303d8b1f7bfdfb082ebcac8fcff6a24af938e3
MarceloYevenesM <marcelo.yevenes.moreno@gmail.com>
2022-04-24 15:34:09 -0400

    feat:details card

--- src/components/ItemPokemon.js
+++ src/components/ItemPokemon.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const ItemPokemon = (pokemon) => {
+  const { id, name, base_experience, height, weight, sprites, stats, types } =
+    pokemon.pokemon;
+
   return (
     <div className="d-flex row vh-100 justify-content-center align-items-center">
       <div className="col-auto text-center">
         <div className="card">
-          <div className="img-top"></div>
-          <div className="card-img-pokemon"></div>
-
+          <Card.Header>Nº{id}</Card.Header>
+          <Card.Img
+            variant="top"
+            src={sprites.front_default}
+            style={{ width: "18rem" }}
+          />
           <div className="card-body text-center">
             <h4 className="card-title"></h4>
+            <h4 className="card-title">{name}</h4>
 
             <Link to={`/`} className="link">
               <Button variant="primary">Volver</Button>
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,7 @@ export const Loading = () => {
   return (
     <div className="d-flex row vh-100 justify-content-center align-items-center">
       <div className="col-auto text-center">
-        <img src={loadingPikachu} alt="Cargando" />
+        <img src={loadingPikachu} alt="Cargando" className="fluid"/>
         <h2>Cargando contenido...</h2>
       </div>
     </div>
